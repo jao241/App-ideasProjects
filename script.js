93ee8052d6fab676bfd7e6110b77683d6ff10982
joao <vorque45@gmail.com>
2021-06-30 11:54:25 -0300

    adicionei um texto para quando valores invalidos forem inseridos

--- script.js
+++ script.js
@@ -3,13 +3,13 @@
 // Valor binário: 10000011
 // Valor decimal: 131
 
-let valorBinario = document.getElementById('entradaInput');
+let entradaBinario = document.getElementById('entradaInput');
 var mensagemErro = 'Somente são permitidos valores numéricos 0 ou 1 \nValor digitado:';
-
+let saidaDecimal = document.getElementById('saidaDecimal');
 
 
 function converteValorBinario() {
- let arrayValores = Array.from(valorBinario.value);
+ let arrayValores = Array.from(entradaBinario.value);
  arrayValores.reverse();
  let valorDecimal = 0;
 
@@ -22,10 +22,11 @@ function converteValorBinario() {
                 valorDecimal += 2 ** x;
             }
         }else{
+            saidaDecimal.value = (`Valor inválido: ${arrayValores[x]}`);
             throw(`${mensagemErro} ${arrayValores[x]}`);
         }
      }
- document.getElementById('saidaDecimal').value = valorDecimal;
+ saidaDecimal.value = valorDecimal;
 }
 
 
