e68e255354b47aa71341c41c38cc3246e35aa50b
joao <vorque45@gmail.com>
2021-06-30 10:59:15 -0300

    conversor

---
+++ script.js
@@ -0,0 +1,28 @@
+// Exemplo do que eu quero
+
+// Valor binário: 10000011
+// Valor decimal: 131
+
+let valorBinario = document.getElementById(entradaInput).innerHTML.toString();
+console.log(valorBinario)
+// var mensagemErro = 'Somente são permitidos valores numéricos 0 ou 1 \nValor digitado:';
+
+// function converteValorBinario() {
+//  let arrayValores = Array.from(valorBinario);
+//  arrayValores.reverse();
+//  let valorDecimal = 0;
+//  for(let x = 0; x < arrayValores.length; x++){
+//         if(arrayValores[x] == "0"|| arrayValores[x] == "1"){
+//             if( x == 0 && arrayValores[x] == "1"){
+//                 valorDecimal = 1;
+//             }
+//             if(arrayValores[x] == "1" && x > 0){
+//                 valorDecimal += 2 ** x;
+//             }
+//         }else
+//         throw(`${mensagemErro} ${arrayValores[x]}`);
+//         console.log(valorDecimal)
+//  }
+// }
+
+// converteValorBinario();
