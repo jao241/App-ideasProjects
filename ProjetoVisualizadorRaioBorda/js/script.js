6169c98a3bd1f7e40c9b4e529fb9a2ca8698f84b
joao <vorque45@gmail.com>
2021-07-26 17:33:09 -0300

    adicionei script

---
+++ ProjetoVisualizadorRaioBorda/js/script.js
@@ -0,0 +1,8 @@
+const entradaEsquerdaAcima = document.getElementById("primeira");
+const entradaDireitaAcima = document.getElementById("segunda");
+const entradaEsquerdaAbaixo = document.getElementById("terceira");
+const entradaDireitaAbaixo = document.getElementById("quarta");
+const todasEntradas = document.getElementsByClassName("entrada");
+const caixa = document.getElementById("caixa");
+
+// caixa.style.borderTopRightRadius = "100px";
